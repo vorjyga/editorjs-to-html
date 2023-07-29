2b8cd583183d54962f5a4af1d0dc698ca8623455
Aleksei Rudenko <a.rudenko@cryptopay.me>
2023-07-29 20:24:43 +0300

    Fix heading id attribute

--- src/modules/header/index.ts
+++ src/modules/header/index.ts
@@ -4,6 +4,6 @@ import './index.css';
 
 export function header ({ data }: Props<HeaderBlock>) {
   const { anchor, level, text } = data;
-  const item = `<h${level} class="ce-header" id="${anchor}">${text}</h${level}>`;
+  const item = `<h${level} class="ce-header" ${anchor ? `id="${anchor}"` : ''}>${text}</h${level}>`;
   return baseBlock(item);
 }
